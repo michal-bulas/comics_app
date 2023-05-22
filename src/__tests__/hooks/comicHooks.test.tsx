f9b63a53f2bc8ad751ec84c24407ee581f0689b0
michal-bulas <michal.bulas1@gmail.com>
2023-05-22 23:50:31 +0200

    add missing part of test previously deleted by mistake

--- src/__tests__/hooks/comicHooks.test.tsx
+++ src/__tests__/hooks/comicHooks.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { renderHook, waitFor } from '@testing-library/react-native';
+import { act, renderHook, waitFor } from '@testing-library/react-native';
 import axios from 'axios';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useComic, useLatestComic, useComics } from '../../hooks/comicHooks';
@@ -7,7 +7,8 @@ import { useComic, useLatestComic, useComics } from '../../hooks/comicHooks';
 jest.mock('axios');
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
-const comicNumber = 123;
+
+const comicData = { num: 123, title: 'Centrifugal' };
 const latestComicData = { num: 2778, title: 'Cuisine' };
 const comicsData = [
 	{ num: 12, title: 'Poisson' },
@@ -18,10 +19,6 @@ const comicsData = [
 ];
 
 describe('comicHooks', () => {
-	beforeEach(() => {
-		jest.useFakeTimers();
-		mockedAxios.get.mockReset();
-	});
 	const wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
 		<QueryClientProvider client={new QueryClient()}>
 			{children}
@@ -29,8 +26,6 @@ describe('comicHooks', () => {
 	);
 
 	it('fetches a comic', async () => {
-		const comicData = { num: comicNumber, title: 'Centrifugal' };
-
 		mockedAxios.get.mockResolvedValueOnce({ data: comicData });
 
 		const { result } = renderHook(() => useComic(comicData.num), { wrapper });
@@ -56,7 +51,15 @@ describe('comicHooks', () => {
 		);
 
 		const { result } = renderHook(() => useComics(12, 16), { wrapper });
-
-		await waitFor(() => result.current.every(({ isSuccess }) => isSuccess));
+		await act(async () => {
+			await waitFor(() => result.current.every(({ isSuccess }) => isSuccess));
+		});
+		// Due to the asynchronous nature of useQueries, the resulting array might not be in the same order as our input
+		// To account for this, we'll sort both arrays by comic number before comparing them
+		expect(
+			result.current
+				.map((queryResult) => queryResult.data)
+				.sort((start, stop) => start.num - stop.num)
+		).toEqual(comicsData.sort((start, stop) => start.num - stop.num));
 	});
 });
